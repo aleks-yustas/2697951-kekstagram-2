eb6b14ef44fb7c425668ef067a4e075de8115917
Ystas <0f4c82@gmail.com>
2026-02-19 22:06:41 +0300

    Добавляет тесты jest

---
+++ js/function.test.js
@@ -0,0 +1,11 @@
+// const isStringLengthValid = require('./functions');
+
+// test('Строка `проверяемая строка` короче 20 символов', () => {
+//   expect(isStringLengthValid('проверяемая строка', 20)).toBe(true);
+// });
+// test('Строка `проверяемая строка` ровно 18 символов', () => {
+//   expect(isStringLengthValid('проверяемая строка', 18)).toBe(true);
+// });
+// test('Строка `проверяемая строка` длиннее 10 символов', () => {
+//   expect(isStringLengthValid('проверяемая строка', 10)).toBe(false);
+// });
