26cbec9b0fc34f512a9b77366d3fbdb8028c6a54
Ystas <0f4c82@gmail.com>
2026-02-19 22:49:48 +0300

    Заменяет тесты на vitest

--- js/function.test.js
+++ js/function.test.js
@@ -1,11 +1,12 @@
-// const isStringLengthValid = require('./functions');
+import { expect, test } from 'vitest';
+import { isStringLengthValid } from './functions';
 
-// test('Строка `проверяемая строка` короче 20 символов', () => {
-//   expect(isStringLengthValid('проверяемая строка', 20)).toBe(true);
-// });
-// test('Строка `проверяемая строка` ровно 18 символов', () => {
-//   expect(isStringLengthValid('проверяемая строка', 18)).toBe(true);
-// });
-// test('Строка `проверяемая строка` длиннее 10 символов', () => {
-//   expect(isStringLengthValid('проверяемая строка', 10)).toBe(false);
-// });
+test('Строка `проверяемая строка` короче 20 символов', () => {
+  expect(isStringLengthValid('проверяемая строка', 20)).toBe(true);
+});
+test('Строка `проверяемая строка` ровно 18 символов', () => {
+  expect(isStringLengthValid('проверяемая строка', 18)).toBe(true);
+});
+test('Строка `проверяемая строка` длиннее 10 символов', () => {
+  expect(isStringLengthValid('проверяемая строка', 10)).toBe(false);
+});
